5342692250df10805ade487b944d92f6140e3349
Jeffrey Musselman <jeffmussels@gmail.com>
2019-08-18 23:19:54 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,9 +3,8 @@ def nyc_pigeon_organizer(data)
   data.each do |attribute, info| 
     info.each do |sub_attribute, names|
       names.each do |name|
-        data.each do |attribute, info|
+        
       sorted_data[name] = {attribute => sub_attribute}
-        end
       end
     end 
   end
